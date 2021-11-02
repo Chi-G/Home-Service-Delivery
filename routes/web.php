ff18d0315edd780ca6d61c125bea3504a04cbbe9
Chijindu Nwokeohuru <chiji4collage@gmail.com>
2021-11-02 01:24:39 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SearchController;
 use App\Http\Livewire\Admin\AdminAddServiceCategoryComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminEditServiceCategoryComponent;
@@ -11,8 +12,19 @@ use App\Http\Livewire\ServicesByCategoryComponent;
 use App\Http\Livewire\Sprovider\SproviderDashboardComponent;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Admin\AdminAddServiceComponent;
+use App\Http\Livewire\Admin\AdminAddSlideComponent;
+use App\Http\Livewire\Admin\AdminContactComponent;
+use App\Http\Livewire\Admin\AdminEditServiceComponent;
+use App\Http\Livewire\Admin\AdminEditSlideComponent;
+use App\Http\Livewire\Admin\AdminServiceProvidersComponent;
 use App\Http\Livewire\Admin\AdminServicesComponent;
 use App\Http\Livewire\Admin\AdminServicesByCategoryComponent;
+use App\Http\Livewire\Admin\AdminSliderComponent;
+use App\Http\Livewire\ChangeLocationComponent;
+use App\Http\Livewire\ContactComponent;
+use App\Http\Livewire\ServiceDetailsComponent;
+use App\Http\Livewire\Sprovider\EditSproviderProfileComponent;
+use App\Http\Livewire\Sprovider\SproviderProfileComponent;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
@@ -33,6 +45,13 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 Route::get('/', HomeComponent::class)->name('home');
 Route::get('/service-categories', ServiceCategoriesComponent::class)->name('home.service_categories');
 Route::get('/{category_slug}/services', ServicesByCategoryComponent::class)->name('home.services_by_category');
+Route::get('/service/{service_slug}', ServiceDetailsComponent::class)->name('home.service_details');
+
+Route::get('/autocomplete', [SearchController::class, 'autocomplete'])->name('autocomplete');
+Route::get('/search', [SearchController::class, 'searchService'])->name('searchService');
+
+Route::get('/change-location', ChangeLocationComponent::class)->name('home.change_location');
+Route::get('/contact-us', ContactComponent::class)->name('home.contact');
 
 //For Customer
 Route::middleware(['auth:sanctum', 'verified'])->group(function(){
@@ -43,6 +62,8 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function(){
 //For Service Provider
 Route::middleware(['auth:sanctum', 'verified', 'authsprovider'])->group(function(){
     Route::get('/sprovider/dashboard', SproviderDashboardComponent::class)->name('sprovider.Dashboard');
+    Route::get('/sprovider/profile', SproviderProfileComponent::class)->name('sprovider.profile');
+    Route::get('/sprovider/profile/edit', EditSproviderProfileComponent::class)->name('sprovider.edit_profile');
 });
 
 
@@ -55,4 +76,12 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
     Route::get('/admin/all-services', AdminServicesComponent::class)->name('admin.all_services');
     Route::get('/admin/{category_slug}/services', AdminServicesByCategoryComponent::class)->name('admin.services_by_category');
     Route::get('/admin/service/add', AdminAddServiceComponent::class)->name('admin.add_service');
+    Route::get('/admin/service/edit/{service_slug}', AdminEditServiceComponent::class)->name('admin.edit_service');
+
+    Route::get('/admin/slider', AdminSliderComponent::class)->name('admin.slider');
+    Route::get('/admin/slide/add', AdminAddSlideComponent::class)->name('admin.add_slide');
+    Route::get('/admin/slide/edit/{slide_id}', AdminEditSlideComponent::class)->name('admin.edit_slide');
+    Route::get('/admin/contacts', AdminContactComponent::class)->name('admin.contacts');
+
+    Route::get('/admin/service-providers', AdminServiceProvidersComponent::class)->name('admin.service_providers');
 });
