0533f3fd6c301c88d07a5f3b3c3c70514f110740
Chijindu Nwokeohuru <chiji4collage@gmail.com>
2021-11-02 05:09:15 +0100

    Add files via upload

--- resources/views/livewire/services-by-category-component.blade.php
+++ resources/views/livewire/services-by-category-component.blade.php
@@ -6,7 +6,7 @@
                 <h1>{{ $scategory->name }} Services</h1>
                 <div class="crumbs">
                     <ul>
-                        <li><a href="index.html">Home</a></li>
+                        <li><a href="/">Home</a></li>
                         <li>/</li>
                         <li>{{ $scategory->name }}</li>
                     </ul>
@@ -27,12 +27,12 @@
         <div class="content_info" style="margin-top: -70px;">
             <div>
                 <div class="container">
-                    <div class="portfolioContainer">
+                    <div class="portfolioContainer">                       
                         @if($scategory->services->count() > 0)
-                        @foreach($scategory->services as $service)                                                
+                        @foreach($scategory->services as $service)
                         <div class="col-xs-6 col-sm-4 col-md-3 nature hsgrids"
                             style="padding-right: 5px;padding-left: 5px;">
-                            <a class="g-list" href="service-details/ac-wet-servicing.html">
+                            <a class="g-list" href="{{ route('home.service_details', ['service_slug' => $service->slug]) }}">
                                 <div class="img-hover">
                                     <img src="{{asset('images/services/thumbnails')}}/{{$service->thumbnail}}" alt="{{$service->name}}"
                                         class="img-responsive">
@@ -41,19 +41,19 @@
                                     <h3>{{$service->name}}</h3>
                                     <hr class="separator">
                                     <p>{{$service->tagline}}</p>
-                                    <div class="content-btn"><a href="service-details/ac-wet-servicing.html"
+                                    <div class="content-btn"><a href="{{ route('home.service_details', ['service_slug' => $service->slug]) }}"
                                             class="btn btn-primary">Book Now</a></div>
                                     <div class="price"><span>&#36;</span><b>From</b>{{$service->price}}</div>
                                 </div>
                             </a>
                         </div>
-                        @endforeach    
+                        @endforeach
                         @else
-                        <div class="col-md-12"><p>There is no Services available.</p></div>
+                        <div class="col-md-12"><p class="text-center">There is no Services available.</p></div>
                         @endif
                     </div>
                 </div>
             </div>
-        </div>            
+        </div>
     </section>
 </div>
