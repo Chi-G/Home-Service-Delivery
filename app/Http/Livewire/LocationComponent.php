8e5b349d061d21914c926f2fabcfdeda07887ba4
Chijindu Nwokeohuru <chiji4collage@gmail.com>
2021-11-02 01:21:24 +0100

    Add files via upload

---
+++ app/Http/Livewire/LocationComponent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LocationComponent extends Component
+{
+    protected $listeners = ['refreshComponent'=>'$refresh'];
+
+    public function render()
+    {
+        return view('livewire.location-component');
+    }
+}
